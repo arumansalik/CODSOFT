4b5f0d421bd0b634cbf1b312e4620846be1ae0aa
unknown <arumansalik45@gmail.com>
2024-02-23 15:07:42 +0530

    first commit

---
+++ lib/quotes.dart
@@ -0,0 +1,184 @@
+String kQuote = 'quote';
+String kAuthor = 'author';
+String anonymous = 'Anonymous';
+
+List quotesList = [
+  {
+    kQuote : 'Education is the most powerful weapon which you can use to change the world',
+    kAuthor : 'Nelson Mandela'
+  },
+  {
+    kQuote : "The function of education is to teach one to think intensively and to think critically. Intelligence plus character – that is the goal of true education",
+    kAuthor: "Martin Luther King, Jr.",
+  },
+  {
+    kQuote : "The roots of education are bitter, but the fruit is sweet.",
+    kAuthor: "Aristotle",
+  },
+  {
+    kQuote: "The whole purpose of education is to turn mirrors into windows.",
+    kAuthor: "Sydney J. Harris",
+  },
+  {
+    kQuote: "You can't litter negativity everywhere and wonder why you have a trashy life.Over thinking ruins you. It ruins the situation, twists things around, makes you worry about futile questions and makes everything much worse than actually it is.",
+    kAuthor: anonymous,
+  },
+  {
+    kQuote: "The purpose of education is to replace an empty mind with an open one.",
+    kAuthor: " Malcolm Forbes",
+  },
+  {
+    kQuote: "The only person who is educated is the one who has learned how to learn and change.",
+    kAuthor: "Carl Rogers",
+  },
+  {
+    kQuote: "Education is the movement from darkness to light.",
+    kAuthor: "Allan Bloom",
+  },
+  {
+    kQuote: "Education is not the filling of a pail, but the lighting of a fire.",
+    kAuthor: 'William Butler Yeats',
+  },
+  {
+    kQuote: "My philosophy is that worrying means you suffer twice.",
+    kAuthor: "J.K. Rowling",
+  },
+  {
+    kQuote: "In every life we have some trouble. But when you worry, you make it double.",
+    kAuthor: 'Bobby McFerrin, Don’t Worry be Happy',
+  },
+  {
+    kQuote: "Empty pockets never held anyone back. Only empty heads and empty hearts can do that.",
+    kAuthor: "Norman Vincent Peale",
+  },
+  {
+    kQuote: "There is nothing that wastes the body like worry, and one who has any faith in God should be ashamed to worry about anything whatsoever.",
+    kAuthor: "Mohandas Gandhi",
+  },
+  {
+    kQuote: "The reason why we struggle with insecurity is because we compare our behind the scenes with everyone else’s highlight reel.",
+    kAuthor: "Stephen Furtick",
+  },
+  {
+    kQuote: "If you want to be happy, do not dwell in the past, do not worry about the future, focus on living fully in the present.",
+    kAuthor: "Roy T. Bennett, The Light in the Heart",
+  },
+  {
+    kQuote: "A day of worry is more exhausting than a week of work.",
+    kAuthor: "John Lubbock",
+  },
+  {
+    kQuote: "In a society, only those who don't have skills complain.",
+    kAuthor: 'Kakashi Hatake, Naruto',
+  },
+  {
+    kQuote: "The man who moves the mountains begins by moving small stones.",
+    kAuthor: "Confucius",
+  },
+  {
+    kQuote: "Over thinking ruins you. Ruins the situation, twists things around, make you worry and just makes everything worse than it actually is.",
+    kAuthor: anonymous,
+  },
+  {
+    kQuote: "The moment you stop chasing happiness, you become happy.",
+    kAuthor: "Sandy Hyatt-James",
+  },
+  {
+    kQuote: "Letting go gives us freedom, and freedom is the only condition for happiness. If, in our heart, we still cling to anything - anger, anxiety, or possessions - we cannot be free.",
+    kAuthor: "Thich Nhat Hanh",
+  },
+  {
+    kQuote: "Unaccepted, the universe has no meaning; it is senseless fate and chaos, but acceptance is a way of discovering meaning, not of manufacturing it.",
+    kAuthor: "Alan W. Watts",
+  },
+  {
+    kQuote: "Always be happy, you never know how happy you can make another by just being happy.",
+    kAuthor: "Ogwo David Emenike",
+  },
+  {
+    kQuote: "Happiness is always there. You just have to choose to see it. There's no point dwelling in the dark and ignoring the light of the stars.",
+    kAuthor: "Carrie Hope Fletcher",
+  },
+  {
+    kQuote: "Happiness is a perfume you cannot pour on others without getting some on yourself",
+    kAuthor: "Ralph Waldo Emerson",
+  },
+  {
+    kQuote: "Letting go gives us freedom, and freedom is the only condition for happiness. If, in our heart, we still cling to anything - anger, anxiety, or possessions - we cannot be free.",
+    kAuthor: "Thich Nhat Hanh",
+  },
+  {
+    kQuote: "A calm and modest life brings more happiness than the pursuit of success combined with constant restlessness.",
+    kAuthor: "Albert Einstein",
+  },
+  {
+    kQuote: "Turn your wounds into wisdom.",
+    kAuthor: "Oprah Winfrey",
+  },
+  {
+    kQuote: "Take responsibility of your own happiness, never put it in other people’s hands.",
+    kAuthor: "Roy T. Bennett",
+  },
+  {
+    kQuote: "Its important to know when to leave , whether its , a Party, a Job, or evening a Relationship. If it aint right , dont stressed it . just walk away peacefully.",
+    kAuthor: "Shaneika Marie",
+  },
+  {
+    kQuote: "Destiny, I feel, is also a relationship - a play between divine grace and willful self-effort.",
+    kAuthor: "Elizabeth Gilbert, Eat, Pray, Love",
+  },
+  {
+    kQuote: "You know what I admire the most in romantic relationships? The couple's ability to express childish expressions.",
+    kAuthor: "Dr.Hong, Doctors",
+  },
+  {
+    kQuote: "Every relationship ends because of distance. Some people are too far, but most are just too close.",
+    kAuthor: anonymous
+  },
+  {
+    kQuote: "If you are driven by fear, anger or pride nature will force you to compete. If you are guided by courage, awareness, tranquility and peace nature will serve you.",
+    kAuthor: "Amit Ray",
+  },
+  {
+    kQuote: "When we have done our best, we should wait the result in peace.",
+    kAuthor: "John Lubbock",
+  },
+  {
+    kQuote: "Letting go gives us freedom, and freedom is the only condition for happiness. If, in our heart, we still cling to anything - anger, anxiety, or possessions - we cannot be free.",
+    kAuthor: "Thich Nhat Hanh",
+  },
+  {
+    kQuote: "Stress and confusion come from being busy. Peace and clarity come from slowing down and stilling your waters.",
+    kAuthor: "Maxime Lagacé",
+  },
+  {
+    kQuote:"If the selflessness of phenomena is analyzed and if this analysis is cultivated, it causes the effect of attaining nirvana. Through no other cause does one come to peace.",
+    kAuthor: anonymous,
+  },
+  {
+    kQuote:"Human happiness and moral duty are inseparably connected.",
+    kAuthor: "George Washington",
+  },
+  {
+    kQuote: "Youth is happy because it has the capacity to see beauty. Anyone who keeps the ability to see beauty never grows old.",
+    kAuthor: "Franz Kafka",
+  },
+  {
+    kQuote: "When we hate our enemies, we are giving them power over us: power over our sleep, our appetites, our blood pressure, our health, and our happiness",
+    kAuthor: "Dale Carnegie,How to Stop Worrying and Start Living",
+  },
+  {
+    kQuote: "Adaptiveness is the shortest road to happiness. Get used to everything around you.",
+    kAuthor: "Mwanandeke Kindembo",
+  },
+  {
+    kQuote: "We are sometimes dragged into a pit of unhappiness by someone else’s opinion that we do not look happy",
+    kAuthor: "Mokokoma Mokhonoana",
+  },
+  {
+    kQuote: "... For without pain, there can be no pleasure. Without sadness, there can be no happiness. Without misery there can be no beauty. And without these, life is endless, hopeless, doomed and damned.Adult. You have become adult.",
+    kAuthor: "Harlan Ellison, Paingod and Other Delusions",
+  }
+
+
+];
